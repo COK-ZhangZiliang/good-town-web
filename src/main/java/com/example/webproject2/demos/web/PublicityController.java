79f1cf7ca8ad5ddc3181e7b1b5b750e13e2cd62b
ziliang <ziliangcok@qq.com>
2024-12-21 20:04:06 +0800

    add cross domain support

--- src/main/java/com/example/webproject2/demos/web/PublicityController.java
+++ src/main/java/com/example/webproject2/demos/web/PublicityController.java
@@ -14,6 +14,7 @@ import java.util.*;
 @RequestMapping("/api/publicity")
 @Getter
 @Setter
+@CrossOrigin
 public class PublicityController {
     @Autowired
     private TownService townService;
--- src/main/java/com/example/webproject2/demos/web/TownController.java
+++ src/main/java/com/example/webproject2/demos/web/TownController.java
@@ -1,6 +1,7 @@
 package com.example.webproject2.demos.web;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/towns")
+@CrossOrigin
 public class TownController {
 
     private final TownService townService;
