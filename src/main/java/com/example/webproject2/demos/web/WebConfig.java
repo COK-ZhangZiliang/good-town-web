86a594466d5fe816c81c3b7f61632854956efbfc
YangChen <1369792882@qq.com>
2024-12-23 19:56:19 +0800

    静态资源访问路径

---
+++ src/main/java/com/example/webproject2/demos/web/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.webproject2.demos.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 静态资源映射：将 /uploads/** 映射到本地上传文件夹
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:D:/program/web/webproject2/uploads/");
+    }
+}
