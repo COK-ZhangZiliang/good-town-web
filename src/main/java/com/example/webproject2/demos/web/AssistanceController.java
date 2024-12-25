e3468d479ff9093f483e57619b2b1d7d5ea9b54d
ziliang <ziliangcok@qq.com>
2024-12-25 15:22:53 +0800

    change default status

--- src/main/java/com/example/webproject2/demos/web/AssistanceController.java
+++ src/main/java/com/example/webproject2/demos/web/AssistanceController.java
@@ -239,7 +239,7 @@ public class AssistanceController {
                 newAssistance.setVideoUrl(null); // 如果为空，保存为 null
             }
 
-            newAssistance.setStatus(3); // 初始状态为 3: 取消（还未发布出去）
+            newAssistance.setStatus(0); // 初始状态为 0: 已发布 未接受
             newAssistance.setCreatedAt(LocalDateTime.now());
             newAssistance.setUpdatedAt(LocalDateTime.now());
 
