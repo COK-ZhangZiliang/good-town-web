f4284a5d4f555ce24bdcfa77d5522f2be3861ff0
ziliang <ziliangcok@qq.com>
2024-12-25 22:49:07 +0800

    add search function

--- src/components/ShowPublicity.vue
+++ src/components/ShowPublicity.vue
@@ -1,9 +1,16 @@
 <!-- 展示所有宣传信息 -->
 <template>
     <div>
+        <div class="search">
+            <el-input v-model="searchQuery" placeholder="搜索..." prefix-icon="Search" clearable
+                @keyup.enter="handleSearch">
+                <template #append>
+                    <el-button @click="handleSearch">搜索</el-button>
+                </template>
+            </el-input>
+        </div>
         <el-loading v-if="loading" />
-        <MultipageComp :data="publicityData" :username="props.username" :type="props.type"
-            @refresh="getAllPublicity" />
+        <MultipageComp :data="publicityData" :username="props.username" :type="props.type" @refresh="getAllPublicity" />
     </div>
 </template>
 
@@ -12,6 +19,7 @@ import axios from 'axios'
 import MultipageComp from '@/components/MultipageComp.vue'
 import { ref, onMounted, defineProps } from 'vue'
 import { getToken } from '@/utils/auth'
+import { ElMessage } from 'element-plus'
 
 // 接收数据
 const props = defineProps({
@@ -27,6 +35,7 @@ const props = defineProps({
 
 const publicityData = ref([])
 const loading = ref(true)
+const searchQuery = ref('')
 
 // 获取所有宣传信息
 const getAllPublicity = async () => {
@@ -72,4 +81,48 @@ const getAllPublicity = async () => {
 onMounted(() => {
     getAllPublicity()
 })
+
+// 处理搜索
+const handleSearch = async () => {
+    if (props.type === "allPromotions" || props.type === "adminQuery") {
+        try {
+            const response = await axios.post('http://10.29.39.146:8088/api/publicity/search', { keyword: searchQuery.value })
+            if (response.data.status === 'success') {
+                console.log(response.data)
+                publicityData.value = response.data.data
+            }
+            else {
+                ElMessage.error(response.data.message)
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    else if (props.type === "myPromotions") {
+        try {
+            const response = await axios.post('http://10.29.39.146:8088/api/publicity/search/my', {
+                token: getToken(),
+                keyword: searchQuery.value
+            })
+            if (response.data.status === 'success') {
+                console.log(response.data)
+                publicityData.value = response.data.data
+            }
+            else {
+                ElMessage.error(response.data.message)
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+}
 </script>
+
+<style scoped>
+.search {
+    flex: 1;
+    max-width: 500px;
+    margin: 20px auto;
+}
+</style>
