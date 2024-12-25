7c244c3c307aeb1a987a44105cf684a43aed024c
ziliang <ziliangcok@qq.com>
2024-12-25 23:06:50 +0800

    add search function

--- src/components/ShowAssistance.vue
+++ src/components/ShowAssistance.vue
@@ -1,6 +1,14 @@
 <!-- 展示所有助力信息 -->
 <template>
     <div>
+        <div class="search">
+            <el-input v-model="searchQuery" placeholder="搜索..." prefix-icon="Search" clearable
+                @keyup.enter="handleSearch">
+                <template #append>
+                    <el-button @click="handleSearch">搜索</el-button>
+                </template>
+            </el-input>
+        </div>
         <el-loading v-if="loading" />
         <MultipageComp :data="assistanceData" :type="props.type" @refresh="getAllAssistance" />
     </div>
@@ -11,9 +19,11 @@ import axios from 'axios'
 import MultipageComp from '@/components/MultipageComp.vue'
 import { ref, onMounted, defineProps } from 'vue'
 import { getToken } from '@/utils/auth'
+import { ElMessage } from 'element-plus'
 
 const assistanceData = ref([])
 const loading = ref(true)
+const searchQuery = ref('')
 
 const props = defineProps({
     type: {
@@ -46,7 +56,7 @@ const getAllAssistance = async () => {
         try {
             loading.value = true
 
-            const response = await axios.get('http://10.29.39.146:8088/api/assistance/my', { headers: { token: getToken() } })
+            const response = await axios.get('http://10.29.39.146:8088/api/assistance/all', { headers: { token: getToken() } })
             if (response.data.status === 'success') {
                 console.log(response.data)
                 assistanceData.value = response.data.data
@@ -65,4 +75,45 @@ const getAllAssistance = async () => {
 onMounted(() => {
     getAllAssistance()
 })
+
+// 处理搜索
+const handleSearch = async () => {
+    if (props.type === 'myAssistance') {
+        try {
+            const response = await axios.post('http://10.29.39.146:8088/api/publicity/search', { keyword: searchQuery.value })
+            if (response.data.status === 'success') {
+                console.log(response.data)
+                assistanceData.value = response.data.data
+            }
+            else {
+                ElMessage.error(response.data.message)
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    else if (props.type === 'allAssistance') try {
+        const response = await axios.post('http://10.29.39.146:8088/api/publicity/search/all', {
+            token: getToken(),
+            keyword: searchQuery.value
+        })
+        if (response.data.status === 'success') {
+            console.log(response.data)
+            assistanceData.value = response.data.data
+        }
+        else {
+            ElMessage.error(response.data.message)
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}
 </script>
+
+<style scoped>
+.search {
+    flex: 1;
+    max-width: 500px;
+    margin: 20px auto;
+}
+</style>
